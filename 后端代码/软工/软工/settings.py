ab10f3c973daaafca4c10479fb1d2c74503f6ea0
甘海 <634831867@qq.com>
2023-12-14 14:48:03 +0800

    update

--- 后端代码/软工/软工/settings.py
+++ 后端代码/软工/软工/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*8=qvu6-ke0&5d@47ax%)23636-mq8s=&r4+=u2@1a&)-etfcs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # 允许跨域
 CORS_ALLOW_ALL_ORIGINS = True
