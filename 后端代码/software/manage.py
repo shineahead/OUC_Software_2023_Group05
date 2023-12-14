3a3768f1721905e24dc3fc00e882fd35aa6c65ac
甘海 <634831867@qq.com>
2023-12-14 14:53:05 +0800

    update

--- 后端代码/software/manage.py
+++ 后端代码/software/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "软工.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "software.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- 后端代码/software/software/asgi.py
+++ 后端代码/software/software/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for 软工 project.
+ASGI config for software project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "软工.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "software.settings")
 
 application = get_asgi_application()
