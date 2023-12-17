36645df4e1a20a465093ce018bf0e06cb7ca775b
甘海 <634831867@qq.com>
2023-12-17 14:05:47 +0800

    update

--- 后端代码/软工/common/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from .common import SARView
-
-urlpatterns = [
-    path('SARDetection/', SARView.as_view())
-]
-
--- 模型训练/模型调用/test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from skimage import io
-
-im1 = io.imread("bern_2.bmp")[:, :, 0].astype(np.float32)
-print(im1.shape)
-
